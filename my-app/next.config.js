335545792ac43055779dffb1300ad3c48034fad2
denial07 <108965893+denial07@users.noreply.github.com>
2025-07-11 23:07:13 +0800

    Update next.config.js

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,10 +1,12 @@
 const isGithubPages = process.env.NODE_ENV === "production";
-
-const repo = "denial07-portfolio"; // Replace with your GitHub repo name
+const repo = "denial07-portfolio"; // GitHub repo name
 
 module.exports = {
-  output: "export",
-  images: { unoptimized: true },
-  basePath: isGithubPages ? `/${repo}` : "",
-  assetPrefix: isGithubPages ? `/${repo}/` : "",
-}
+  output: "export", // Needed for static HTML export (GitHub Pages requires this)
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during builds
+  },
+  images: { unoptimized: true }, // Required if using `next/image` with `next export`
+  basePath: isGithubPages ? `/${repo}` : "", // Ensures routing works on GitHub Pages
+  assetPrefix: isGithubPages ? `/${repo}/` : "", // Ensures assets load correctly
+};
